a45a764c4f22eae122bed601ce6318ecdff65b90
GANESH-NAYUDU <ganeshnaiduvuppalapati@gmail.com>
2024-10-23 10:01:01 +0530

    test

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-//hello ganesh
+const express = require("express");
+const path = require("path");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const port = process.env.PORT || 4000;
+
+const app = express();
+app.use(express.json());
+
+dbPath = path.join(__dirname, "userData.db");
+
+let db = null;
+
+const initializeDBandServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(port, () => {
+      console.log("SERVER RUNNING SUCCESFULLY......");
+    });
+  } catch (e) {
+    console.log(`DB ERROR ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBandServer();
+
+///JWT AUTHENTICATION
+const authenticateToken = (request, response, next) => {
+  let jwtToken;
+  const authHeader = request.headers["authorization"];
+  if (authHeader !== undefined) {
+    jwtToken = authHeader.split(" ")[1];
+  }
+  if (jwtToken === undefined) {
+    response.status(401);
+    response.send("Invalid JWT Token");
+  } else {
+    jwt.verify(jwtToken, "MY_SECRET_TOKEN", async (error, payload) => {
+      if (error) {
+        response.status(401);
+        response.send("Invalid JWT Token");
+      } else {
+        next();
+      }
+    });
+  }
+};
+///
